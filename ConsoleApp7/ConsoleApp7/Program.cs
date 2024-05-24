c1af92ad97920a6ba33930efc9a591c0ddd14940
Zynnon <74718472+Zynnon@users.noreply.github.com>
2024-05-24 13:57:26 +0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number to convert: ");
+            int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+            string binary = ConvertDecimalToBinary(decimalNumber);
+
+            Console.WriteLine("Binary: {0}", binary);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static string ConvertDecimalToBinary(int decimalNumber)
+        {
+            string binary = "";
+
+            while (decimalNumber > 0)
+            {
+                int remainder = decimalNumber % 2;
+                binary = remainder + binary;
+                decimalNumber /= 2;
+            }
+            return binary;
+
+            
+        }
+
+        
+    }
+}
