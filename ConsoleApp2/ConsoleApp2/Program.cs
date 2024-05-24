ce65213dbc79e4308989d996ee10b6e23b89f79e
Zynnon <74718472+Zynnon@users.noreply.github.com>
2024-05-24 13:54:36 +0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreAndDisplayNumbers
+{
+    class Program
+    {
+          static void Main(string[] args)
+        {
+            List<float> numbers = new List<float>();
+            float number;
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                string input = Console.ReadLine();
+                number = Convert.ToSingle(input);
+
+                // Check if the number ends in two zeros
+                if (number % 100 == 0)
+                {
+                    break;
+                }
+                numbers.Add(number);
+            }
+            Console.WriteLine("You entered the following numbers:");
+            foreach (float num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
