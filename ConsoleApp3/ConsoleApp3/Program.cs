5b44e7b6bce5b1f852aa85cb7bee3e91a15059cc
Zynnon <74718472+Zynnon@users.noreply.github.com>
2024-05-24 13:55:25 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterchangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Before interchange:");
+            Console.WriteLine("First number: {0}", number1);
+            Console.WriteLine("Second number: {0}", number2);
+
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine(" ");
+            Console.WriteLine("After interchange:");
+            Console.WriteLine("First number: {0}", number1);
+            Console.WriteLine("Second number: {0}", number2);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
