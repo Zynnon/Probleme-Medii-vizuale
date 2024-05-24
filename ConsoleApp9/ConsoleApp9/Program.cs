b7fe52ed4f6e0a349b9c8678b7fdaa8aa02b7ed2
Zynnon <74718472+Zynnon@users.noreply.github.com>
2024-05-24 13:58:29 +0300

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PowerCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToThePowerOf(2, 3)); // Output: 8
+            Console.WriteLine(ToThePowerOf(5, 5)); // Output: 3125
+
+
+            string input2 = Console.ReadLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static int ToThePowerOf(int baseNumber, int exponent)
+        {
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= baseNumber;
+            }
+            return result;
+        }
+    }
+    
+}
