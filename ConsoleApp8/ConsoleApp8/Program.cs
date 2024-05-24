9519390b180a0f4786acd0f0a71047dc5d150b18
Zynnon <74718472+Zynnon@users.noreply.github.com>
2024-05-24 13:58:04 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the operation (+, -, x, /): ");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case 'x':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized character.");
+                    return;
+            }
+
+            Console.WriteLine("Result: {0}", result);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            string input2 = Console.ReadLine();
+        }
+    }
+}
